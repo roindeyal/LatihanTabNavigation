79684d2b5acfc33c6e5c2e9b12e1d9d0c47124c6
root <root@localhost.localdomain>
2019-12-02 04:37:02 +0700

    Latihan Tab Navigation With One Fragment

--- app/src/main/java/com/svafvel/software/production/mytablayout/HomeFragment.java
+++ app/src/main/java/com/svafvel/software/production/mytablayout/HomeFragment.java
@@ -3,11 +3,13 @@ package com.svafvel.software.production.mytablayout;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,9 +17,18 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private static final String ARG_SECTION_NUMBER = "section number";
 
     public HomeFragment() {
         // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance(int index){
+        HomeFragment fragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return  fragment;
     }
 
 
@@ -26,6 +37,21 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState){
+
+        super.onViewCreated(view, savedInstanceState);
+        final TextView textView = view.findViewById(R.id.section_label);
+
+        int index = 1;
+        if(getArguments() != null){
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+
+        textView.setText(getString(R.string.content_tab_text) + " " + index);
+
     }
 
 }
